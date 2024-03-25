1ae992cb3c2caaa1e66f990bd1b1ef03ca3fe6d7
allis <avf5633@psu.edu>
2024-03-24 22:45:02 -0400

    started lab 11

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,29 @@
 """ Create PyGameAIPlayer class here"""
-
+import random
+import pygame
 
 class PyGameAIPlayer:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def selectAction(self, state):
+        return random.randit(0, 9)
 
 
 """ Create PyGameAICombatPlayer class here"""
 
 
 class PyGameAICombatPlayer:
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+        """self.initial_weapon = 1
+        self.agentGuess=-1
+        self.round=0
+        self.opponentchoices=[]
+        self.mychoices=[] """
+
+    def weapon_selecting_strategy(self):
+        while True:
+            self.weapon=random.randint(0,2)
+            return self.weapon
+
