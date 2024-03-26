28622d56e7e3d9e49e10f89d8c0df3a752c44529
aww_bees <avf5633@psu.edu>
2024-03-26 10:57:44 -0400

    episode one

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -9,3 +9,15 @@ Note that observation/state is a tuple of the form (player1_health, player2_heal
 Action is simply the weapon selected by the player.
 Reward is the reward for the player for that turn.
 '''
+from lab11.pygame_combat import run_turn
+from lab11.turn_combat import Combat
+
+def run_episode(player1, player2):
+    season_one=[]
+    currentGame=Combat()
+    while not currentGame.gameOver():
+        run_turn(currentGame, player1, player2)
+        health_tuple=(player1.health, player2.health)
+        reward=1 if player1.health>player2.health else 0
+        season_one.append((player1.weapon, health_tuple, reward))
+    return season_one
